558355dc3bf451f4a7242b61f17c05143c66cf47
Vinit kashyap <vinitkumar10062004@gmail.com>
2026-06-07 14:26:53 +0530

    new pattern questions

--- src/pattern2.java
+++ src/pattern2.java
@@ -1,8 +1,8 @@
-public class pattern1 {
-    public static void main(String args[]) {
+public class pattern2 {
+    public static void main(String[] args){
         int n = 5;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < i+1; j++){
                 System.out.print("* ");
             }
             System.out.println();
--- src/pattern5.java
+++ src/pattern5.java
@@ -1,8 +1,8 @@
-public class pattern1 {
-    public static void main(String args[]) {
+public class pattern5 {
+    public static void main(String[] args){
         int n = 5;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = n; j > i+1; j--){
                 System.out.print("* ");
             }
             System.out.println();
